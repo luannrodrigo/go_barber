c3179ef3a5af030378f7083c5854dbaf9af0ca60
Luann Rodrigo <luann.rodrigos@gmail.com>
2019-11-11 22:19:54 -0300

    configurando multer, criando controllers para os uploads de arquivos, criando migrations para criação das tabelas para os avatares e seus relacionamentos

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize';
 
+// importando models
 import User from '../app/models/User';
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+// carregando modelos
+const models = [User, File];
 
 class Database {
 	constructor() {
@@ -14,7 +17,9 @@ class Database {
 	init() {
 		this.connection = new Sequelize(databaseConfig);
 
-		models.map(model => model.init(this.connection));
+		models
+			.map(model => model.init(this.connection))
+			.map(model => model.associete && model.associete(this.connection.models));
 	}
 }
 
