d6391757d75faafab044ad765dddd32e72a2ccc4
Luann Rodrigo <luann.rodrigos@gmail.com>
2019-12-27 10:23:13 -0300

    Feature: Create config to send email when an appointment is cancelled

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,17 +1,11 @@
 import * as Yup from 'yup';
-import {
-	startOfHour,
-	parseISO,
-	isBefore,
-	format,
-	sub,
-	subHours,
-} from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
 	async index(req, res) {
@@ -129,7 +123,15 @@ class AppointmentController {
 	}
 
 	async delete(req, res) {
-		const appointment = await Appointment.findByPk(req.params.id);
+		const appointment = await Appointment.findByPk(req.params.id, {
+			include: [
+				{
+					model: User,
+					as: 'provider',
+					attributes: ['name', 'email'],
+				},
+			],
+		});
 
 		if (appointment.user_id !== req.userId) {
 			return res.status(401).json({
@@ -156,6 +158,12 @@ class AppointmentController {
 		appointment.cancelled_at = new Date();
 
 		await appointment.save();
+
+		await Mail.sendMail({
+			to: `${appointment.provider.name} <${appointment.provider.email}>`,
+			subject: 'Agendamento cancelado',
+			text: 'Você tem um novo cancemalento',
+		});
 
 		return res.json(appointment);
 	}
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: '54b1e01e150140',
+		pass: '6027d38fbfdcde',
+	},
+	default: {
+		from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+	},
+};
